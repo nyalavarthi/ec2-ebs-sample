9357f03c24b2249db7732bb5564fecfa0d55e349
Narendra Yalavarthi <nyalavarthi@gmail.com>
2020-01-07 13:53:07 -0500

    Update ec2-ebs-volume.tf

--- ec2-ebs-volume.tf
+++ ec2-ebs-volume.tf
@@ -50,6 +50,11 @@ resource "aws_volume_attachment" "ebs_att" {
   device_name = "/dev/sdh"
   volume_id   = "${aws_ebs_volume.example.id}"
   instance_id = "${aws_instance.web.id}"
+  tags = {
+    Name        = "NY-test-EBS"
+    Environment = "Sandbox",
+    Owner       = "Narendra Yalavarthi"
+  }
 }
 
 output "amiids" {
